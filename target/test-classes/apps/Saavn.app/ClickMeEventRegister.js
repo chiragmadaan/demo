82120597c1f30ebba09e095448eaec32e8838312
Chirag Madaan <chiragm.3315@gmail.com>
2020-04-21 08:44:24 +1000

    Updating .gitignore

--- target/test-classes/apps/Saavn.app/ClickMeEventRegister.js
+++
@@ -1,5 +0,0 @@
-var button = document.getElementById("btn_submit");
-button.addEventListener("click", function() {
-            var messageToPost = {'buttonID':'btn_submit'};
-            window.webkit.messageHandlers.buttonClicked.postMessage(messageToPost);
-        },false);
